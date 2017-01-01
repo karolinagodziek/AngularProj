09c68759feaa7892927cb271e67bea410b7de20b
StolarczykJakub <Stolarczyk.kuba@gmail.com>
2017-01-01 23:54:00 +0100

    first commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angularProjekt', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
